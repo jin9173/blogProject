b101d4b133b2b68f59d6120bb6c6a760dbe56164
jin9173 <jinhappier2@gmail.com>
2020-01-29 23:14:06 +0900

    blog의 post_archive_year.html

--- blog/urls.py
+++ blog/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
     # Example: /blog/archive/
     path('archive/', views.PostAV.as_view(), name='post_archive'),
-    # Example: /blog/archive/2019/
+    # Example: /blog/archive/2020/
     path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
-    # Example: /blog/archive/2019/nov/
+    # Example: /blog/archive/2020/nov/
     path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
-    # Example: /blog/archive/2019/nov/10/
+    # Example: /blog/archive/2020/nov/10/
     path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
     # Example: /blog/archive/today/
     path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
