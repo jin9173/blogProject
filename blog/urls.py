566c8739e339142704291e75f415ec18bf1d5c7c
jin9173 <jinhappier2@gmail.com>
2020-01-29 23:01:52 +0900

    blog의 post_archive.html

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # Example: /blog/post/ (same as /blog/)
     path('post/', views.PostLV.as_view(), name='post_list'),
     # Example: /blog/post/django-example/
-    re_path(r'^post(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
     # Example: /blog/archive/
     path('archive/', views.PostAV.as_view(), name='post_archive'),
     # Example: /blog/archive/2019/
